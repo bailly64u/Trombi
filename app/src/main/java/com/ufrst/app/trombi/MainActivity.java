60eba8551848675eb2f470ed4bfe5d8eeddc3e07
ninjinski <louis.bailly1@etu.univ-lorraine.fr>
2019-11-07 16:50:24 +0100

    Initial-commit

---
+++ app/src/main/java/com/ufrst/app/trombi/MainActivity.java
@@ -0,0 +1,55 @@
+package com.ufrst.app.trombi;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+        setListeners();
+    }
+
+    // Désérialise les vues dont on aura besoin depuis le XML
+    private void findViews(){
+        mNavigationView = findViewById(R.id.NAV_navigationView);
+    }
+
+    // Applique des listeners sur certains éléments
+    private void setListeners(){
+
+        // Menu hamburger latéral
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                switch(item.getItemId()){
+                    case R.id.NAV_item1:
+                        Toast.makeText(MainActivity.this, "Salut1", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.NAV_item2:
+                        Toast.makeText(MainActivity.this, "Salut2", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.NAV_item3:
+                        Toast.makeText(MainActivity.this, "Salut3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true; // true to display the item as the selected item [android dev]
+            }
+        });
+    }
+}
