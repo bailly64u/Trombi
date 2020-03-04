2502d56510d56df4be63bf9b96f0a7e03c7d5604
ninjinski <louis.bailly1@etu.univ-lorraine.fr>
2020-03-04 11:10:15 +0100

    Nettoyage du code - model

--- app/src/main/java/com/ufrst/app/trombi/model/EleveWithGroupsImport.java
+++ app/src/main/java/com/ufrst/app/trombi/model/EleveWithGroupsImport.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class EleveWithGroupsImport {
 
-    private List<Groupe> dbGroupes;             // Liste des groupes avec leurs ids en DB
     private List<String> groupes;               // Liste des noms des groupes
     private Eleve eleve;
 
@@ -18,25 +17,11 @@ public class EleveWithGroupsImport {
         this.eleve = eleve;
     }
 
-    // Retourne des entités Room pour les insérer dans la BD
-//    public List<EleveWithGroups> getEleveWithGroups(List<Groupe> dbGroupes){
-//        List<EleveWithGroups> eleveWithGroups = new ArrayList<>();
-//
-//        for(String groupName : groupes){
-//            if(groupes.contains())
-//            eleveWithGroups.add()
-//        }
-//    }
-
     public List<String> getGroups(){
         return groupes;
     }
 
     public Eleve getEleve(){
         return eleve;
-    }
-
-    public void setDbGroupes(List<Groupe> dbGroupes){
-        this.dbGroupes = dbGroupes;
     }
 }
